f2f5f9879e5b5b32997d8a448f27a7b3e0feb12c
andersy005 <axbanihirwe@ualr.edu>
2016-04-07 23:00:28 -0500

    SetValue function: determines cell index and updates other cells

--- spreadsheet.js
+++ spreadsheet.js
@@ -14,6 +14,76 @@ jQuery.fn.sheet = function(options){
 	t = t + '<thead class="ui-widget-header"><tr class="ui-helper-reset"><th></th>';
 
 	for (i=0; i <opts.cols; i++){
-		t = t + '<th class="ui-helper-reset">'+ String.fromCharCode(65+i)+"</th";
+		t = t + '<th class="ui-helper-reset">'+ String.fromCharCode(65+i)+"</th>";
+	}
+
+	t = t + '</tr></thead><tbody class="ui-widget-content">';
+	for (i=0, i < opts.rows; i++){
+		t = t + '<tr class="ui-helper-reset"><td class ="rowindex ui-helper-reset ui-widget-header">'+ (i+1)+"</td>";
+		for(j=0; j<opts.cols;j++){
+			id = String.fromCharCode(65+j)+(i+1)
+			t = t + '<td class="cell ui-helper-reset ui-widget-content" id="'+id+'"><span class="formula ui-helper-reset-hidden"></span></td>';
+			/* create a global variable */
+			window[id]=0
+		}
+
+		t = t + "</tr>";
+	}
+
+	t = t +"<tbody></table>";
+	this.append(t);
+
+	function setvalue(value, settings){
+		/*determine cell index and update global var */
+		currentcell = $(this).attr('id');
+		currentresult = eval(value);
+
+		if(typeof(currentresult)== 'undefined'){
+			currentresult ='#undef';
+			window[currentcell] = 0;
+		}else{
+			window[currentcell] = currentresult;
+		}
+
+		/*update all other cells */
+
+		var changed;
+		var depth = 0;
+		do{
+			depth ++;
+			changed = false;
+			$('.sheet').find('.cell').each(function(index, element){
+				cell = $(element).attr('id');
+				if (currentcell != cell){
+					span = $(element).children('span').first();
+					orig = window[cell];
+					window[cell] = 0;
+					formula = span.text();
+					if(formula.length > 0){
+						result = eval(formula);
+
+						if(result != orig){ changed = true;}
+						if(typeof(result) == 'undefined'){
+							result='#undef';
+						}else{
+							window[cell]= result;
+						}
+					}else{
+						result = ' ';
+					}
+
+					if(opts.logging){log (depth+': '+cell+' '+formula+'='+result+'['+orig+']<br>');}
+					/* replaceWith() would replace the td element itself and using the text() function on the td remoces its span child (at least in IE8)*/
+					$(element).empty().append('<span class="formula ui-helper-hidden replaced">'+ formula+'</span>'+result);
+				}
+			});
+		}while(changed && (depth < opts.cols* opts.rows));
+		if(depth >= opts.cols * opts.rows) currentresult = '#Circular!';
+		return ('<span class="formula ui-helper-hidden">'+value+'</span>'+currentresult);
+
+
+		}
+
+		function
 	}
 }
